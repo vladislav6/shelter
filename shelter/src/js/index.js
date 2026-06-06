ebce9483e7c6cd8a8fa434f34b86614764f0dcb6
Vladislav <vlad.kvan96@gmail.com>
2026-06-06 14:51:46 +0300

    feat: add self-assessment in console.log

---
+++ shelter/src/js/index.js
@@ -0,0 +1,17 @@
+import '../scss/index.scss';
+
+console.log(`
+Main page (70 / 70)
+- Markup validation (10 / 10)
+- Layout matches the design (35 / 35)
+Each block/section is checked independently - issues from a previous block don't carry over.
+- CSS requirements (15 / 15)
+- Interactivity (10 / 10)
+
+Pets page (40 / 40)
+- Markup validation (10 / 10)
+- Layout matches the design (15 / 15)
+Each block/section is checked independently - issues from a previous block don't carry over.
+- CSS requirements (5 / 5)
+- Interactivity (10 / 10)
+`);
